aebadc9b902d2c8dafc1e76605847b621be89d04
Cloyz2021 <84689194+Cloyz2021@users.noreply.github.com>
2025-02-23 21:13:40 -0500

    ++

--- Source/Samples/Restir/GBuffer.cpp
+++ Source/Samples/Restir/GBuffer.cpp
@@ -2,7 +2,23 @@
 
 namespace Restir
 {
+const std::string kDepthName = "depth";
+
+const ChannelList kGBufferChannels = {
+    // clang-format off
+    { "posW",           "gPosW",            "Position in world space",                           true /* optional */, ResourceFormat::RGBA32Float },
+    { "normW",          "gNormW",           "Shading normal in world space",                     true /* optional */, ResourceFormat::RGBA32Float },
+    { "tangentW",       "gTangentW",        "Shading tangent in world space (xyz) and sign (w)", true /* optional */, ResourceFormat::RGBA32Float },
+    { "faceNormalW",    "gFaceNormalW",     "Face normal in world space",                        true /* optional */, ResourceFormat::RGBA32Float },
+    { "texC",           "gTexC",            "Texture coordinate",                                true /* optional */, ResourceFormat::RG32Float   },
+    { "texGrads",       "gTexGrads",        "Texture gradients (ddx, ddy)",                      true /* optional */, ResourceFormat::RGBA16Float },
+    { "mvec",           "gMotionVector",    "Motion vector",                                     true /* optional */, ResourceFormat::RG32Float   },
+    { "mtlData",        "gMaterialData",    "Material data (ID, header.x, header.y, lobes)",     true /* optional */, ResourceFormat::RGBA32Uint  },
+    // clang-format on
+};
+
 GBuffer::GBuffer(ref<Device> pDevice, uint32_t width, uint32_t height)
+: RenderPass(pDevice)
 {
     createTextures(pDevice, width, height);
     initializeGraphicStates(pDevice);
@@ -10,6 +26,10 @@ GBuffer::GBuffer(ref<Device> pDevice, uint32_t width, uint32_t height)
 
 void GBuffer::createTextures(ref<Device> pDevice, uint32_t width, uint32_t height)
 {
+    // Set witdh and height
+    mWidth = width;
+    mHeight = height;
+
     // Create gbuffer textures.
     mPositionWsTexture = pDevice->createTexture2D(
         width,
@@ -103,6 +123,13 @@ void GBuffer::createTextures(ref<Device> pDevice, uint32_t width, uint32_t heigh
 
 void GBuffer::initializeGraphicStates(ref<Device> pDevice)
 {
+    if (!pDevice->isShaderModelSupported(ShaderModel::SM6_2))
+        FALCOR_THROW("GBuffer requires Shader Model 6.2 support.");
+    if (!pDevice->isFeatureSupported(Device::SupportedFeatures::Barycentrics))
+        FALCOR_THROW("GBuffer requires pixel shader barycentrics support.");
+    if (!pDevice->isFeatureSupported(Device::SupportedFeatures::RasterizerOrderedViews))
+        FALCOR_THROW("GBuffer requires rasterizer ordered views (ROVs) support.");
+
     mDepthPass.pState = GraphicsState::create(pDevice);
     mGBufferPass.pState = GraphicsState::create(pDevice);
 
@@ -112,6 +139,25 @@ void GBuffer::initializeGraphicStates(ref<Device> pDevice)
     mGBufferPass.pState->setDepthStencilState(pDsState);
 
     mpFbo = Fbo::create(pDevice);
+}
+
+RenderPassReflection GBuffer::reflect(const CompileData& compileData)
+{
+    RenderPassReflection reflector;
+
+    // Add the required depth output. This always exists.
+    reflector.addOutput(kDepthName, "Depth buffer")
+        .format(ResourceFormat::D32Float)
+        .bindFlags(ResourceBindFlags::DepthStencil)
+        .texture2D(mWidth, mHeight);
+
+    // Add all the other outputs.
+    // The default channels are written as render targets, the rest as UAVs as there is way to assign/pack render targets yet.
+    addRenderPassOutputs(reflector, kGBufferChannels, ResourceBindFlags::RenderTarget, sz);
+   // addRenderPassOutputs(reflector, kGBufferExtraChannels, ResourceBindFlags::UnorderedAccess, sz);
+    //reflector.getField(kVBufferName)->format(mVBufferFormat);
+
+    return reflector;
 }
 
 void GBuffer::render(RenderContext* pRenderContext)
--- Source/Samples/Restir/GBuffer.h
+++ Source/Samples/Restir/GBuffer.h
@@ -2,11 +2,13 @@
 
 namespace Restir
 {
-class GBuffer
+class GBuffer : public RenderPass
 {
 public:
     GBuffer(ref<Device> pDevice, uint32_t width, uint32_t height);
     void render(RenderContext* pRenderContext);
+
+    RenderPassReflection reflect(const CompileData& compileData) override;
 
     inline const ref<Texture>& getPositionWsTexture() const { return mPositionWsTexture; }
     inline const ref<Texture>& getNormalWsTexture() const { return mNormalWsTexture; }
@@ -20,6 +22,9 @@ public:
 private:
     void createTextures(ref<Device> pDevice, uint32_t width, uint32_t height);
     void initializeGraphicStates(ref<Device> pDevice);
+
+    uint32_t mWidth;
+    uint32_t mHeight;
 
     ref<Texture> mPositionWsTexture;
     ref<Texture> mNormalWsTexture;
