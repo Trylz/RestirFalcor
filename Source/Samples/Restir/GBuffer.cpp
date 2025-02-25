b64a229901cccac7021ff0c1edc67fa8fea3f3b6
Cloyz2021 <84689194+Cloyz2021@users.noreply.github.com>
2025-02-25 00:05:16 -0500

    +++

---
+++ Source/Samples/Restir/GBuffer.cpp
@@ -0,0 +1,86 @@
+#include "GBuffer.h"
+#include "Core/Pass/RasterPass.h"
+
+namespace Restir
+{
+
+GBuffer::GBuffer(ref<Device> pDevice, uint32_t width, uint32_t height) : mpDevice(pDevice), mWidth(width), mHeight(height)
+{
+    createTextures();
+}
+
+void GBuffer::createTextures()
+{
+    // Create gbuffer textures.
+    mPositionWsTexture = mpDevice->createTexture2D(
+        mWidth, mHeight, ResourceFormat::RGBA32Float, 1, 1, nullptr, ResourceBindFlags::ShaderResource | ResourceBindFlags::UnorderedAccess
+    );
+
+    mNormalWsTexture = mpDevice->createTexture2D(
+        mWidth, mHeight, ResourceFormat::RGBA32Float, 1, 1, nullptr, ResourceBindFlags::ShaderResource | ResourceBindFlags::UnorderedAccess
+    );
+
+    mAlbedoTexture = mpDevice->createTexture2D(
+        mWidth, mHeight, ResourceFormat::RGBA32Float, 1, 1, nullptr, ResourceBindFlags::ShaderResource | ResourceBindFlags::UnorderedAccess
+    );
+
+    mSpecularTexture = mpDevice->createTexture2D(
+        mWidth, mHeight, ResourceFormat::RGBA32Float, 1, 1, nullptr, ResourceBindFlags::ShaderResource | ResourceBindFlags::UnorderedAccess
+    );
+
+    mMotionVectorTexture = mpDevice->createTexture2D(
+        mWidth, mHeight, ResourceFormat::RGBA32Float, 1, 1, nullptr, ResourceBindFlags::ShaderResource | ResourceBindFlags::UnorderedAccess
+    );
+}
+
+void GBuffer::compilePrograms()
+{
+    // We'll now create a raytracing program. To do that we need to setup two things:
+    // - A program description (ProgramDesc). This holds all shader entry points, compiler flags, macro defintions,
+    // etc.
+    // - A binding table (RtBindingTable). This maps shaders to geometries in the scene, and sets the ray generation and
+    // miss shaders.
+    //
+    // After setting up these, we can create the Program and associated RtProgramVars that holds the variable/resource
+    // bindings. The Program can be reused for different scenes, but RtProgramVars needs to binding table which is
+    // Scene-specific and needs to be re-created when switching scene. In this example, we re-create both the program
+    // and vars when a scene is loaded.
+
+    auto shaderModules = mpScene->getShaderModules();
+    auto typeConformances = mpScene->getTypeConformances();
+
+    // Get scene defines. These need to be set on any program using the scene.
+    auto defines = mpScene->getSceneDefines();
+
+    ProgramDesc rasterProgDesc;
+    rasterProgDesc.addShaderModules(shaderModules);
+    rasterProgDesc.addShaderLibrary("Samples/Restir/GBuffer.slang").vsEntry("vsMain").psEntry("psMain");
+    rasterProgDesc.addTypeConformances(typeConformances);
+
+    mpFbo = Fbo::create(mpDevice);
+    mpRasterPass = RasterPass::create(mpDevice, rasterProgDesc, defines);
+}
+
+void GBuffer::render(RenderContext* pRenderContext, ref<Scene> pScene)
+{
+    if (pScene != mpScene)
+    {
+        mpScene = pScene.get();
+        compilePrograms();
+    }
+
+    auto var = mpRtVars->getRootVar();
+
+    var["PerFrameCB"]["viewportDims"] = float2(mWidth, mHeight);
+    var["PerFrameCB"]["sampleIndex"] = mSampleIndex++;
+
+    var["gPositionWs"] = mPositionWsTexture;
+    var["gNormalWs"] = mNormalWsTexture;
+    var["gAlbedo"] = mAlbedoTexture;
+    var["gSpecular"] = mSpecularTexture;
+    var["gMotionVector"] = mMotionVectorTexture;
+
+    mpScene->raytrace(pRenderContext, mpRaytraceProgram.get(), mpRtVars, uint3(mWidth, mHeight, 1));
+}
+
+} // namespace Restir
---
+++ Source/Samples/Restir/GBuffer.h
@@ -0,0 +1,37 @@
+#include "../RenderPasses/GBuffer/GBuffer/GBufferRaster.h"
+
+namespace Restir
+{
+class GBuffer /*: public RenderPass*/
+{
+public:
+    GBuffer(ref<Device> pDevice, uint32_t width, uint32_t height);
+    void render(RenderContext* pRenderContext, ref<Scene> pScene);
+
+    inline const ref<Texture>& getPositionWsTexture() const { return mPositionWsTexture; }
+    inline const ref<Texture>& getNormalWsTexture() const { return mNormalWsTexture; }
+    inline const ref<Texture>& getAlbedoTexture() const { return mAlbedoTexture; }
+    inline const ref<Texture>& getSpecularTexture() const { return mSpecularTexture; }
+    inline const ref<Texture>& getMotionVectorTexture() const { return mMotionVectorTexture; }
+
+private:
+    void createTextures();
+    void compilePrograms();
+
+    ref<Device> mpDevice;
+    Scene* mpScene = nullptr; // TODO use smart pointer
+
+    uint32_t mWidth;
+    uint32_t mHeight;
+
+    ref<Fbo> mpFbo;
+
+    ref<Texture> mPositionWsTexture;
+    ref<Texture> mNormalWsTexture;
+    ref<Texture> mAlbedoTexture;
+    ref<Texture> mSpecularTexture;
+    ref<Texture> mMotionVectorTexture;
+
+    ref<RasterPass> mpRasterPass;
+};
+} // namespace Restir
