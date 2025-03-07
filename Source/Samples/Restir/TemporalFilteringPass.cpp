a0c463c8f9da7072480e9f3e40933e60ef83937e
Trylz <nebularender@gmail.com>
2025-03-07 16:38:51 -0500

    Update TemporalFilteringPass.cpp

--- Source/Samples/Restir/TemporalFilteringPass.cpp
+++ Source/Samples/Restir/TemporalFilteringPass.cpp
@@ -32,6 +32,7 @@ void TemporalFilteringPass::render(Falcor::RenderContext* pRenderContext)
     var["PerFrameCB"]["sampleIndex"] = ++mSampleIndex;
     var["PerFrameCB"]["motion"] = (uint)(mPreviousFrameViewProjMat != mpScene->getCamera()->getViewProjMatrix());
 
+    var["PerFrameCB"]["temporalLinearDepthThreshold"] = SceneSettingsSingleton::instance()->temporalLinearDepthThreshold;
     var["PerFrameCB"]["temporalWsRadiusThreshold"] = SceneSettingsSingleton::instance()->temporalWsRadiusThreshold;
     var["PerFrameCB"]["temporalNormalThreshold"] = SceneSettingsSingleton::instance()->temporalNormalThreshold;
 
